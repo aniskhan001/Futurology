022a9981e49c3b9d5a6fcefbf1a960366e458a14
Anisuzzaman Khan <aniskhan001@outlook.com>
2016-11-16 19:56:08 +0600

    slack webhook integration

--- bot.py
+++ bot.py
@@ -1,27 +1,27 @@
 import praw
 import time
+import datetime
+import slackweb
 
 r = praw.Reddit(user_agent = "/r/Futurology/ scraper by aniskhan001")
+s = slackweb.Slack(url="https://hooks.slack.com/services/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")
 
 cache = []
-subs = []
 
 def run_bot():
-	the_time = time.time()
-	submissions = r.get_subreddit('Futurology').get_hot()
+	the_time = time.mktime( datetime.datetime.utcnow().timetuple() )
+	submissions = r.get_subreddit('Futurology').get_hot(limit=3)
 
 	for sub in submissions:
 		# The Top post is over 15 hours old OR less than 750 upvotes
-		if ( the_time - sub.created > 54000 or sub.score < 750):
+		if ( the_time - sub.created_utc > 54000 or sub.score < 750):
 			if sub.id in cache:
 				continue
 			else:
 				cache.append(sub.id)
-				print sub.id
-				print sub.url
-				# print sub.title
+				hour_diff = int( (the_time-sub.created_utc)/3600 )
+				s.notify(text = sub.title + "\nThere's an open position on /r/Futurology! The top post has " + str(sub.score) + " points and was posted " + str( hour_diff ) + " hours ago.")
 
 while True:
 	run_bot()
-	print 'Time to sleep!'
-	time.sleep(30*60) # 30 minutes call interval
+	time.sleep(2*60) # 2 minutes call interval
