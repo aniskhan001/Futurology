4f32c578a680480307e6941b86ab0249a2c20ae8
Anisuzzaman Khan <aniskhan001@outlook.com>
2016-11-17 21:58:26 +0600

    indent

--- bot.py
+++ bot.py
@@ -16,9 +16,9 @@ def run_bot():
 	for sub in submissions:
 		# The Top post is over 15 hours old OR less than 750 upvotes
 		if ( (the_time - sub.created_utc > 54000 or sub.score < 750) and (sub.id not in cache) ):
-				cache.append(sub.id)
-				hour_diff = int( (the_time-sub.created_utc)/3600 )
-				s.notify(text = "There's an open position on <"+ sub.permalink +"|/r/Futurology!> The top post has *" + str(sub.score) + " points* and was posted *" + str( hour_diff ) + " hours* ago.")
+			cache.append(sub.id)
+			hour_diff = int( (the_time-sub.created_utc)/3600 )
+			s.notify(text = "There's an open position on <"+ sub.permalink +"|/r/Futurology!> The top post has *" + str(sub.score) + " points* and was posted *" + str( hour_diff ) + " hours* ago.")
 
 while True:
 	run_bot()
