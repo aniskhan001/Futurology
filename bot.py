a7f592ba807b37e650c15ac67e28c78f84d1bd9e
Anisuzzaman Khan <aniskhan001@outlook.com>
2016-11-17 22:34:48 +0600

    user options and documentation added

--- bot.py
+++ bot.py
@@ -4,22 +4,50 @@ import datetime
 import slackweb
 from os import environ
 
-r = praw.Reddit(user_agent = "/r/Futurology/ scraper by aniskhan001")
-s = slackweb.Slack(url = environ.get('WEBHOOK_URL'))
+
+####################
+### user options ###
+####################
 
 cache = []
+the_limit = 3 # how many top submissions (max) per call, reddit default order
+call_interval = 10 # time interval to run the bot again, in minutes
+the_subreddit = 'Futurology' # your chosen subreddit
+
+# An user agent is important. Change according to your needs.
+r = praw.Reddit(user_agent = "/r/" + the_subreddit + "/ scraper by aniskhan001")
+# it's a good practice to save the WEBHOOK_URL in an environment variable so we don't expose the URL
+s = slackweb.Slack(url = environ.get('WEBHOOK_URL'))
+
+
+######################
+### bot definition ###
+######################
 
 def run_bot():
+
 	the_time = time.mktime( datetime.datetime.utcnow().timetuple() )
-	submissions = r.get_subreddit('Futurology').get_hot(limit=1)
+	submissions = r.get_subreddit(the_subreddit).get_hot(limit = the_limit)
 
 	for sub in submissions:
-		# The Top post is over 15 hours old OR less than 750 upvotes
+		"""
+		A sample logic is added. You can always build your own logic
+		For accessing different members of PRAW object, the /sample_data file is given as reference
+
+		"""
+
+		# The Top post is over 15 hours old OR less than 750 upvotes | 15*60*60 = 54000 seconds
 		if ( (the_time - sub.created_utc > 54000 or sub.score < 750) and (sub.id not in cache) ):
+
+			# cahing the item so we don't post it twice
 			cache.append(sub.id)
-			hour_diff = int( (the_time-sub.created_utc)/3600 )
-			s.notify(text = "There's an open position on <"+ sub.permalink +"|/r/Futurology!> The top post has *" + str(sub.score) + " points* and was posted *" + str( hour_diff ) + " hours* ago.")
+			hour_diff = int( (the_time - sub.created_utc) / 3600 )
+
+			# Learn more about messaging format: https://api.slack.com/docs/message-formatting
+			message_text = "There's a new post on <"+ sub.permalink +"|/r/" + the_subreddit + "!> posted *" + str(hour_diff) + " hours* ago with: *" + str(sub.score) + "* upvotes!"
+			s.notify(text = message_text)
+
 
 while True:
 	run_bot()
-	time.sleep(30*60) # 30 minutes call interval
+	time.sleep(call_interval*60)
