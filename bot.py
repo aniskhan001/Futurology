855f1bd8e7bb6b846369b46714505bb1162f2bdb
Anisuzzaman Khan <aniskhan001@outlook.com>
2016-11-17 21:25:39 +0600

    send only top post

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import time
 import datetime
 import slackweb
 from os import environ
-# from pymongo import MongoClient
 
 r = praw.Reddit(user_agent = "/r/Futurology/ scraper by aniskhan001")
 s = slackweb.Slack(url = environ.get('WEBHOOK_URL'))
@@ -12,22 +11,15 @@ cache = []
 
 def run_bot():
 	the_time = time.mktime( datetime.datetime.utcnow().timetuple() )
-	submissions = r.get_subreddit('Futurology').get_hot(limit=10)
+	submissions = r.get_subreddit('Futurology').get_hot(limit=1)
 
 	for sub in submissions:
 		# The Top post is over 15 hours old OR less than 750 upvotes
-		if ( the_time - sub.created_utc > 54000 or sub.score < 750):
-			if sub.id in cache:
-				continue
-			else:
+		if ( (the_time - sub.created_utc > 54000 or sub.score < 750) and (sub.id not in cache) ):
 				cache.append(sub.id)
 				hour_diff = int( (the_time-sub.created_utc)/3600 )
-				s.notify(text = sub.title + "\nThere's an open position on /r/Futurology! The top post has *" + str(sub.score) + " points* and was posted *" + str( hour_diff ) + " hours* ago.")
-				time.sleep(30) # 30 seconds delay
-				# print "There's an open position on /r/Futurology! The top post has *" + str(sub.score) + " points* and was posted *" + str( hour_diff ) + " hours* ago."
+				s.notify(text = sub.title + "\nThere's an open position on <"+ sub.permalink +"|/r/Futurology!> The top post has *" + str(sub.score) + " points* and was posted *" + str( hour_diff ) + " hours* ago.")
 
 while True:
 	run_bot()
-	# s.notify(text = "sleeping....")
-	# print "sleeping..."
 	time.sleep(30*60) # 30 minutes call interval
